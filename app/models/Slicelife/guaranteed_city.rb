e688a02928f4691f173d53d5b5c988537db74a94
captproton <carl@wdwhub.net>
2020-08-07 15:11:20 -0700

    CHANGE: assigned a city from the City model to each Eatery when importing slicelife

---
+++ app/models/Slicelife/guaranteed_city.rb
@@ -0,0 +1,10 @@
+class Slicelife::GuaranteedCity
+    # def initialize
+        
+    # end
+        #  city           = Slicelife::GuaranteedCity.new.find(city:  parsed_address.fetch(:city), state_abbr: parsed_address.fetch(:state_abbr))
+    def find(city, state_abbr)
+        city = City.where(city_ascii: city).where(state_id: state_abbr).first || Slicelife::MissingCity.new
+    end
+
+end
--- app/models/Slicelife/importer.rb
+++ app/models/Slicelife/importer.rb
@@ -18,10 +18,31 @@ class Slicelife::Importer
                 max_wait:       shop.max_wait_time,
                 delivery:       shop.delivery,                
                 source_url:     shop.link_href,
-                city: City.last
+                city:           _find_city(shop.address)
             )
             eatery.save!
         end
     end
+
+    def _find_city(address)
+        parsed_address  = _parse_address(address)
+        city            = Slicelife::GuaranteedCity.new.find(parsed_address.fetch(:city), parsed_address.fetch(:state_abbr))
+        if city.city_ascii == "not found"
+            city = City.find_or_initialize_by(city_ascii: "not found") do |c|
+                c.state_id          = city.state_id
+                c.state_name        = city.state_name
+                c.county_name_all   = city.county_name_all
+            end
+            city.save!
+        end
+        city
+    end
+
+    def _parse_address(address)
+        city        = address.split(", ")[1]
+        state_abbr  = address.split(", ")[2]
+        {city: city, state_abbr: state_abbr}
+    end
+
 
 end
