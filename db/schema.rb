da978328a5651c0004b38fb9140f96b95ff68a94
captproton <carl@wdwhub.net>
2021-04-02 23:24:47 -0700

    FIXED: remnants of a bad commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_08_11_152724) do
-=======
 ActiveRecord::Schema.define(version: 2020_08_23_023952) do
->>>>>>> feature/hubspot_connection
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
